d4756573e4ba8b296836c646fe8c94aeb1dd811f
elroykanye <elroykanye@gmail.com>
2021-10-19 00:02:37 +0100

    ek : added ./src/app/services/auth.service.ts

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {LoginRequest, RegisterRequest} from '../models/requests/auth.request';
+
+const AUTH_API = 'http://localhost:8080/api/auth';
+const SIGN_IN_ENDPOINT = 'signin';
+const SIGN_UP_ENDPOINT = 'signup';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) {}
+
+  login(loginRequest: LoginRequest): Observable<any> {
+    return this.http.post(
+      // TODO check to be sure this endpoint is valid
+      AUTH_API + SIGN_IN_ENDPOINT, {
+        loginRequest
+      }, httpOptions
+    );
+  }
+
+  register(registerRequest: RegisterRequest): Observable<any> {
+    return this.http.post(
+      AUTH_API + SIGN_UP_ENDPOINT, {
+        registerRequest
+      }, httpOptions
+    );
+  }
+}
